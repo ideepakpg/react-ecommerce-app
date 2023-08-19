660640bd6e1ba4df654a1848acf6717d350d2871
Deepak PG <deepakpg121@gmail.com>
2023-08-19 19:56:29 +0530

    Insert logout and home path

--- src/LayoutRoutes/Routes.jsx
+++ src/LayoutRoutes/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import HomePage from '../components/Home/HomePage';
+import Logout from '../components/Logout/Logout';
 
 function Routes1() {
     return (
@@ -12,6 +13,7 @@ function Routes1() {
                 <Header />
                 <Routes>
                     <Route path='/' element={<HomePage />} />
+                    <Route path='/logout' element={<Logout />} />
                 </Routes>
                 <Footer />
             </Router>
