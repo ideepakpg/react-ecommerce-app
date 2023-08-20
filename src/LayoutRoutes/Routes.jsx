ffc848e12e769e7b28d04c4b37e8cfbc30f3fb1f
Deepak PG <deepakpg121@gmail.com>
2023-08-20 19:10:56 +0530

    Route product details path

--- src/LayoutRoutes/Routes.jsx
+++ src/LayoutRoutes/Routes.jsx
@@ -4,6 +4,7 @@ import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 import HomePage from '../components/Home/HomePage';
 import Logout from '../components/Logout/Logout';
+import ProductDetails from '../components/ProductDetails/ProductDetails';
 
 function Routes1() {
     return (
@@ -14,6 +15,7 @@ function Routes1() {
                 <Routes>
                     <Route path='/' element={<HomePage />} />
                     <Route path='/logout' element={<Logout />} />
+                    <Route path='/product/:id' element={<ProductDetails />} />
                 </Routes>
                 <Footer />
             </Router>
